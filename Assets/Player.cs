c357a4387bb9a507be9f7c030015603ffad4b6e8
L45zl0 <wiktormaternik@gmail.com>
2023-04-01 18:36:22 +0200

    Imported TMPro, created Player and PlayerHP scripts and Objects

---
+++ Assets/Player.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHP = 6, currentHP = 6;
+
+}
---
+++ Assets/PlayerHP.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+
+public class PlayerHP : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI text;
+    [SerializeField]
+    private Player player;
+
+    public void Start()
+    {
+        text.text = string.Format("{0}/{1}", player.currentHP.ToString(), player.maxHP.ToString() ); 
+    }
+}
